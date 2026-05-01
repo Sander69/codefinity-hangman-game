e2212cdce90a322c0fbe15d729cf2d4cf5379493
Sander69 <>
2026-05-01 16:13:26 +0000

    Preparing the Word List and Base Structure

---
+++ main.py
@@ -0,0 +1,17 @@
+# List of words used for the Hangman game
+WORDS = [___]
+
+# Symbol used to represent hidden letters
+HIDDEN_SYMBOL = "_"
+
+# Maximum number of incorrect guesses allowed
+MAX_GUESSES = ___
+
+
+def main():
+    # Entry point for the game
+    pass
+
+
+if __name__ == "__main__":
+    main()
